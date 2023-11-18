8100d1efa8b1527f9fec8e69a9a87db7e0b205be
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2023-11-18 15:13:50 +0100

    fixed dockerfile api

--- app/components/filtergroup/FilterGroup.tsx
+++ app/components/filtergroup/FilterGroup.tsx
@@ -7,7 +7,6 @@ const FilterGroup = async ({name = "group", groups = []}) => {
 
   return (
     <div>
-<<<<<<< HEAD
       <div className='flex flex-row'>
         <ToggleButton />
         {name}
@@ -21,14 +20,6 @@ const FilterGroup = async ({name = "group", groups = []}) => {
             </div>)
         })}
       </div>
-=======
-      {/*{name}*/}
-      {/*<div className='ml-2'>*/}
-      {/*  {groups.map(element => {*/}
-      {/*    return (<div>{element}</div>)*/}
-      {/*  })}*/}
-      {/*</div>*/}
->>>>>>> cc1b883f22a13c47e3a4918521d5b60ab0fe3e1e
     </div>
   )
 }
