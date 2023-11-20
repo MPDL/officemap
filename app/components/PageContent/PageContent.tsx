dadd6606d188964e2207ed8ac0a7601380676ee8
Felix Riehm <mail@felixriehm.de>
2023-11-20 15:56:10 +0100

    move comment

--- app/components/PageContent/PageContent.tsx
+++ app/components/PageContent/PageContent.tsx
@@ -2,6 +2,10 @@ import React, { Dispatch, SetStateAction } from 'react'
 import Filter from '../Filter/Filter'
 import dynamic from 'next/dynamic'
 
+// Followed instructions at https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading#with-no-ssr
+// In import('path/to/component'), the path must be explicitly written. It can't be a template string nor a variable.
+// This is needed otherwise LeafletMap will be rendered server side which does not work because leaflet relies on the
+// 'window' object.
 const DynamicLeafletMap = dynamic(() => import('../LeafletMap/LeafletMap'), {
     ssr: false,
     loading: () => <div>Loading map...</div>,
