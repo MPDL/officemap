b659efc71d2949d2ddf7e089f22a1992443d5906
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2023-12-13 15:18:42 +0100

    closing button for filter

--- app/components/Filter/Filter.tsx
+++ app/components/Filter/Filter.tsx
@@ -1,19 +1,31 @@
-import React, { ReactNode } from 'react'
+'use client'
+import React, { ReactNode, useState } from 'react'
 
 interface Props {
   children: ReactNode;
 }
 
 const Filter = ({children} : Props) => {
+	var [visible, setVisible] = useState(true)
+
+	const onClick = () => {
+		setVisible(!visible);
+	}
+
   return (
     <div className='absolute top-5 right-5 flex flex-col shadow-xl rounded-3xl p-4 z-[999], bg-white'>
-      <div className='flex flex-row justify-between'>
-        <div className='text-xl font-bold'> Filter </div>
-        <div> X </div>
-      </div>
-      <div> 
-        {children}
-      </div>
+			{ visible ?  
+				<>
+				<div className='flex flex-row justify-between'>
+					<div className='text-xl font-bold'> Filter </div>
+					<button className='symbol text-xl' onClick={onClick}>close </button>
+				</div>
+				<div> 
+					{children}
+				</div></> 
+				:
+				<button className='symbol' onClick={onClick}>tune</button>
+			}
     </div>
   )
 }
